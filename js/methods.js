27265d645d81f8a01fc0bdd4cb33b24dbc7ee80f
Anupam Dahal <miracleanupam@gmail.com>
2025-01-19 13:30:09 -0500

    dark mode 1.0

--- js/methods.js
+++ js/methods.js
@@ -1,10 +1,21 @@
 window.addEventListener("load", (event) => {
-  
   const hamburger = document.getElementById('hamburger');
   const sideNav = document.getElementById('side-nav');
   const closeSideNav = document.getElementById('close-side-nav');
   const containerDiv = document.getElementsByClassName('container')[0];
   const overlay = document.getElementById('overlay');
+  const lightIcon = document.getElementById("light-icon");
+  const darkIcon = document.getElementById("dark-icon");
+
+  const isDarkMode = document.body.classList.contains("dark-mode");
+  console.log(isDarkMode);
+  // Toggle icon visibility
+  lightIcon.style.display = isDarkMode ? "inline" : "none";
+  darkIcon.style.display = isDarkMode ? "none" : "inline";
+
+
+
+
 
   hamburger.addEventListener('click', (event) => {
     sideNav.style.display = 'flex';
@@ -23,4 +34,27 @@ window.addEventListener("load", (event) => {
   homeBtn.addEventListener('click', (event) => {
     window.scrollTo(0, 0);
   })
+
+  const toggleButton = document.getElementById("theme-toggle");
+  toggleButton.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+
+    // Save preference in localStorage
+    if (document.body.classList.contains("dark-mode")) {
+      localStorage.setItem("theme", "dark");
+      lightIcon.style.display =  "inline";
+      darkIcon.style.display = "none";
+    } else {
+      lightIcon.style.display =  "none";
+      darkIcon.style.display = "inline";
+      localStorage.setItem("theme", "light");
+    }
+  });
+
+  // Load saved theme on page load
+  if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark-mode");
+    lightIcon.style.display = isDarkMode ? "none" : "inline";
+    darkIcon.style.display = isDarkMode ? "inline" : "none";
+  }
 })
